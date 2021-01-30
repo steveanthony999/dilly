6300beb7341fa136040521d6437df698a811cf63
Steven Woodward <steveanthony999@gmail.com>
2021-01-29 23:37:19 -0800

    styled sign up form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import SignUp from './components/SignUp';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Dilly</h1>
-      <SignUp />
-    </div>
+    <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <SignUp />
+      </div>
+    </Container>
   );
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,32 +1,37 @@
 import { useRef } from 'react';
+import { Card, Button, Form } from 'react-bootstrap';
 
 const SignUp = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordconfirmationRef = useRef();
+  const passwordConfirmRef = useRef();
 
   return (
-    <div>
-      <div>
-        <h2>Sign Up</h2>
-        <form>
-          <div>
-            <label htmlFor=''>email</label>
-            <input type='email' name='' id='' ref={emailRef} />
-          </div>
-          <div>
-            <label htmlFor=''>password</label>
-            <input type='password' name='' id='' ref={passwordRef} />
-          </div>
-          <div>
-            <label htmlFor=''>Password Confirmation</label>
-            <input type='password' name='' id='' ref={passwordconfirmationRef} />
-          </div>
-          <button type='submit'>Sign Up</button>
-        </form>
-      </div>
-      <div>Already have an account? Log In</div>
-    </div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Sign Up</h2>
+          <Form>
+            <Form.Group id='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type='email' ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id='password-confirm'>
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type='password' ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button className='w-100' type='submit'>
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>Already have an account? Log In</div>
+    </>
   );
 };
 
